6886a2cf00f77c3303190bad434a935e9c77c842
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-12-09 17:07:47 -0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>STEM Made Fun</title>
+        <meta name="og:title" content="STEM Made Fun"></meta>
+        <meta name="og:description" content="STEM Made Fun website for code for cause"></meta>
+      </Head>
       <body style={{overflowX: "hidden"}}>
           <Main />
           <NextScript />
